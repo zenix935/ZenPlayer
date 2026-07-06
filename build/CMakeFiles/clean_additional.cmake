c18344e47a981f2a2aaf6eadec62f43aef98074a
zenix935 <smootharian@gmail.com>
2026-07-06 16:41:13 +0330

    God Please Help me

--- build/CMakeFiles/clean_additional.cmake
+++ build/CMakeFiles/clean_additional.cmake
@@ -3,10 +3,10 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
-  "CMakeFiles\\QtVsCodeApp_autogen.dir\\AutogenUsed.txt"
-  "CMakeFiles\\QtVsCodeApp_autogen.dir\\ParseCache.txt"
-  "CMakeFiles\\ZenPlayer_autogen.dir\\AutogenUsed.txt"
-  "CMakeFiles\\ZenPlayer_autogen.dir\\ParseCache.txt"
+  [[CMakeFiles\QtVsCodeApp_autogen.dir\AutogenUsed.txt]]
+  [[CMakeFiles\QtVsCodeApp_autogen.dir\ParseCache.txt]]
+  [[CMakeFiles\ZenPlayer_autogen.dir\AutogenUsed.txt]]
+  [[CMakeFiles\ZenPlayer_autogen.dir\ParseCache.txt]]
   "QtVsCodeApp_autogen"
   "ZenPlayer_autogen"
   )
