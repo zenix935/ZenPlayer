1cb843239dcf60bed28522d96e1f097de168f551
zenix935 <smootharian@gmail.com>
2026-07-08 04:07:11 +0330

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ZenPlayer_FILE C:/MINE/codes/ZenPlayer/build/ZenPlayer.exe)
-set(__QT_DEPLOY_TARGET_ZenPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ZenPlayer_RUNTIME_DLLS C:/Qt/6.11.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Multimediad.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Concurrentd.dll;C:/Qt/6.11.1/msvc2022_64/bin/Qt6Cored.dll)
